dad9d00247e5de4347328a33165f2adbd740189c
okuiyuki <okui0909@gmail.com>
2026-07-04 13:24:08 +0900

    下の余白追加

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -8,7 +8,7 @@ type AppLayoutProps = {
 function AppLayout({ children }: AppLayoutProps) {
   return (
     <div
-      className="mx-auto flex h-svh w-full max-w-md flex-col overflow-hidden bg-cover bg-center bg-no-repeat px-6 py-10"
+      className="mx-auto flex h-svh w-full max-w-md flex-col overflow-hidden bg-cover bg-center bg-no-repeat px-6 pt-10 pb-20"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       {children}
